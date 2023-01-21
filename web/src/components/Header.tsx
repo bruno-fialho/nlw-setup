04835e0c0a9bdbc9c280f50225a4bb12c0ab364a
Bruno Fialho <brugerf@gmail.com>
2023-01-21 19:53:12 -0300

    feat: web completed

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -14,7 +14,7 @@ export function Header() {
       <Dialog.Root>
         <Dialog.Trigger 
           type="button"
-          className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300"
+          className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300 transition-colors focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-background"
         >
           <Plus size={20} className="text-violet-500" />
           Novo Hábito
@@ -30,7 +30,7 @@ export function Header() {
 
             <NewHabitForm />
 
-            <Dialog.Close className="absolute right-6 top-6 text-zinc-400 hover:text-zinc-200">
+            <Dialog.Close className="absolute right-6 top-6 text-zinc-400 hover:text-zinc-200 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900">
               <X size={24} aria-label="Fechar" />
             </Dialog.Close>
           </Dialog.Content>
